41e10361fb98463e37e493ec8218771eab4c90c4
leon <978651195@qq.com>
2019-02-28 21:54:29 +0800

    start

---
+++ apostrophe.py
@@ -0,0 +1,2 @@
+message = "One of Pthon's strengths is its diverse community"
+print(message)
---
+++ hello_world.py
@@ -0,0 +1,7 @@
+message = "hello world!"
+name = "leon"
+print("my name is "+name+"\n"+message)
+print(name.upper())
+print(name.lower())
+print(name.title())
+print("my name is "+name.title())
